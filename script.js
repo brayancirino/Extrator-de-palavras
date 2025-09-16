544b42a81a2a951593c0e364b01626e5f17dade4
Brayan Cirino <brayan.cirino@escola.pr.gov.br>
2025-09-16 09:33:34 -0300

    AULA 4

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-
 const botaomostrarpalavras = document . querySelector("#botao-palavrachave");
 
 botaomostrarpalavras.addEventListener("click", mostrapalavraschave);
 
 function mostrapalavraschave() {
- const texto = document.querySelector("#entrada-de-texto").value;
-
- const camporesultado = document.querySelector("#resultado-palavrachavve")
-
- const palavras = texto.split(" ");
-
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const camporesultado = document.querySelector("#resultado-palavrachavve")
+    const palavrachave =processaTexto(texto);
+ 
  camporesultado.textContent = palavras;
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
 }
